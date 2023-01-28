52081f4399ea7b785981e9f0591fc8f304da66dd
Oleg Sidorov <sidoroffoleg@mail.ru>
2023-01-28 14:34:04 +0300

    Tests added

--- src/test/java/ProductManagerTest.java
+++ src/test/java/ProductManagerTest.java
@@ -29,7 +29,7 @@ class ProductManagerTest {
         Assertions.assertArrayEquals(expected,actual);
     }
     @Test
-    public void testSearch() {
+    public void testSearchMany() {
         ProductRepository repo = new ProductRepository();
         ProductManager manager = new ProductManager(repo);
         Book book1 = new Book(1, "Harry Potter and Philosopher's Stone", 300, "J. K. Rowling");
@@ -50,4 +50,46 @@ class ProductManagerTest {
         assertArrayEquals(expected,actual);
     }
 
+    @Test
+    public void testSearchOne() {
+        ProductRepository repo = new ProductRepository();
+        ProductManager manager = new ProductManager(repo);
+        Book book1 = new Book(1, "Harry Potter and Philosopher's Stone", 300, "J. K. Rowling");
+        Book book2 = new Book(2, "Harry Potter and the Chamber of Secrets", 300, "J. K. Rowling");
+        Book book3 = new Book(3, "War and Peace", 300, "Leo Tolstoy");
+        Smartphone smartphone1 = new Smartphone(4, "iPhone 14",100000,"Apple");
+        Smartphone smartphone2 = new Smartphone(5, "Galaxy A52",80000,"Samsung");
+
+        manager.add(book1);
+        manager.add(book2);
+        manager.add(book3);
+        manager.add(smartphone1);
+        manager.add(smartphone2);
+
+        Product[] actual = manager.searchBy("iPhone");
+        Product[] expected = {smartphone1};
+
+        assertArrayEquals(expected,actual);
+    }
+    @Test
+    public void testSearchNone() {
+        ProductRepository repo = new ProductRepository();
+        ProductManager manager = new ProductManager(repo);
+        Book book1 = new Book(1, "Harry Potter and Philosopher's Stone", 300, "J. K. Rowling");
+        Book book2 = new Book(2, "Harry Potter and the Chamber of Secrets", 300, "J. K. Rowling");
+        Book book3 = new Book(3, "War and Peace", 300, "Leo Tolstoy");
+        Smartphone smartphone1 = new Smartphone(4, "iPhone 14",100000,"Apple");
+        Smartphone smartphone2 = new Smartphone(5, "Galaxy A52",80000,"Samsung");
+
+        manager.add(book1);
+        manager.add(book2);
+        manager.add(book3);
+        manager.add(smartphone1);
+        manager.add(smartphone2);
+
+        Product[] actual = manager.searchBy("1984");
+        Product[] expected = {};
+
+        assertArrayEquals(expected,actual);
+    }
 }
